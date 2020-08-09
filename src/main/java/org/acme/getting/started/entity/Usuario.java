e679acf0ea2579e8cb6082df33ced2c70e485684
LucasNascimento <lucaspnascimento@hotmail.com>
2020-08-09 17:23:20 -0300

    update create default table on database in correct schema

--- src/main/java/org/acme/getting/started/entity/Usuario.java
+++ src/main/java/org/acme/getting/started/entity/Usuario.java
@@ -5,6 +5,7 @@ import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import javax.persistence.*;
 
 @Entity
+@Table(name = "USUARIO", schema = "SCHEMA_USER")
 public class Usuario extends PanacheEntityBase {
 
     @Id
